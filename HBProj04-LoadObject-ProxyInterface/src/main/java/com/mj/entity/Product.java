bfcead9f1f0890bdd87f9ddceb7548740174b2f3
Manoj <manojkumarjena2022@gmail.com>
2023-11-28 22:11:27 +0530

    Load Object Using ProxyInterface

---
+++ HBProj04-LoadObject-ProxyInterface/src/main/java/com/mj/entity/Product.java
@@ -0,0 +1,15 @@
+package com.mj.entity;
+
+import lombok.Data;
+
+@Data
+public final class Product implements IProduct {
+	private Integer pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+	public Product()
+	{
+		System.out.println("Product.Product() -- "+this.getClass());
+	}
+}
